bc0402c4a2e54c8805eeb9b052fcc26aa09a044e
mrpintu-dev1 <mrpintudev1@gmail.com>
2022-02-22 09:03:05 +0600

    fronted private route and protected login implement

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,53 @@ import Login from './pages/Login';
 import Registration from './pages/Registration';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
+import PrivateRoute from './components/auth/PrivateRoute';
+import ProtectedLogin from './components/auth/ProtectedLogin';
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<Dashboard/>} />
-                <Route path='/login' element={<Login/>} />
-                <Route path='/register' element={<Registration/>} />
-                <Route path='/forgot-password' element={<ForgotPassword/>} />
-                <Route path='/reset-password/:resetToken' element={<ResetPassword/>} />
+                <Route
+                    path="/"
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <ProtectedLogin>
+                            <Login />
+                        </ProtectedLogin>
+                    }
+                />
+                <Route
+                    path="/register"
+                    element={
+                        <ProtectedLogin>
+                            <Registration />
+                        </ProtectedLogin>
+                    }
+                />
+                <Route
+                    path="/forgot-password"
+                    element={
+                        <ProtectedLogin>
+                            <ForgotPassword />
+                        </ProtectedLogin>
+                    }
+                />
+                <Route
+                    path="/reset-password/:resetToken"
+                    element={
+                        <ProtectedLogin>
+                            <ResetPassword />
+                        </ProtectedLogin>
+                    }
+                />
             </Routes>
         </Router>
     );
